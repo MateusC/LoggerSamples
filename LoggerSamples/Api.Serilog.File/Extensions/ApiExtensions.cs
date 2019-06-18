a42405a81f902c9ba599862a9a3e2e11ad80a584
MateusC <mateus.martino@gmail.com>
2019-06-17 23:44:08 -0300

    Adicionado projeto de log sendo escrito em arquivos.

---
+++ LoggerSamples/Api.Serilog.File/Extensions/ApiExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Api.Serilog.File
+{
+    public static class ApiExtensions
+    {
+        public static void AddLogger(this IServiceCollection services, IConfiguration configuration)
+        {
+            Log.Logger = new LoggerConfiguration()
+               .ReadFrom.Configuration(configuration)
+               .CreateLogger();
+        }
+    }
+}
